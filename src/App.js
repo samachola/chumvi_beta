f6b9e45d9cddcae53b3855b1d7fcc96fc0bc542b
Sam Achola <sam.achola@live.com>
2018-03-18 10:44:16 +0300

    Set up routes and login component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import LoginPage from './components/pages/LoginPage';
 
 const App = () => (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Route path='/' exact component={LoginPage} />
       </div>);
 
 export default App;
---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoginPage = () => (
+  <div>
+    <h2>Welcome Home</h2>
+  </div>
+);
+
+export default LoginPage;
