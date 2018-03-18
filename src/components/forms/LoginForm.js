f6abc0cb76f2cbf5b9d7daacb0a1f69c8a9354f6
Sam Achola <sam.achola@live.com>
2018-03-18 12:27:34 +0300

    Login form with client-side validation

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Validator from 'validator';
+import propTypes from 'proptypes';
+
+class LoginForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: {
+                email: '',
+                password: ''
+            },
+            loading: false,
+            errors: {}
+        }
+
+        this.onChange = this.onChange.bind(this);
+    }
+    
+
+    onChange = (e) => this.setState({
+        data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        const errors = this.validate(this.state.data);
+        this.setState({ errors });
+        if(Object.keys(errors).length === 0) {
+            this.props.submit(this.state.data);
+        }
+    }
+
+    validate = (data) => {
+        const errors = {};
+        
+        if(!Validator.isEmail(data.email)) errors.email = 'Not a valid email address';
+        if(!data.password) errors.password = "Password Field Is Required";
+
+        return errors;
+    }
+
+    render(){
+        const { data, errors } = this.state;
+        return (
+            <form onSubmit={ this.onSubmit }>
+                <div className="inputwrapper">
+                        <label htmlFor="email">
+                            Email 
+                            { errors.email && <span className="errs">{ errors.email }</span> }
+                        </label>
+                        <input type="text" value={data.email} name="email" id="email" onChange={this.onChange} />
+                </div>
+
+                <div className="inputwrapper">
+                        <label htmlFor="password">
+                            Password 
+                            { errors.password && <span className="errs">{ errors.password }</span> }
+                        </label>
+                        <input 
+                            type="password" 
+                            value={data.password} 
+                            name="password"  
+                            id="password" 
+                            onChange={this.onChange} />
+                </div>
+
+                <div className="login-action">
+                        <input type="submit" value="login" />
+                        <Link to="/">Forgot Password..?</Link>
+                </div>
+            </form>
+        );
+    }
+}
+
+LoginForm.propTypes = {
+    submit: propTypes.func.isRequired
+}
+
+export default LoginForm;
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import bg from '../video/bg.mp4' ;
+import LoginForm from '../forms/LoginForm';
 
-const LoginPage = () => (
-  <div>
-    <h2>Welcome Home</h2>
-  </div>
-);
+class LoginPage extends Component {
+
+  submit = (data) => {
+    console.log(data);
+  }
+  render(){
+    return (
+      <div className="ch-login">
+        <div className="intro">
+          <video loop muted autoPlay className="fullscreen-bg__video">
+            <source src={bg} type="video/mp4" />
+          </video>
+        </div>
+
+        <div className="forms">
+          <div className="logo">
+            <h2>chumvi</h2>
+          </div>
+
+          <div className="form">
+            <div className="tab">
+              <Link className="active" to="/">login</Link>
+      	  		<Link to="/">signup</Link>
+            </div>
+            <LoginForm submit={this.submit} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 export default LoginPage;
