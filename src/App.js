fb4d89cac527adc37ad2bd4e10b2324304a0d788
Sam Achola <sam.achola@live.com>
2018-03-18 10:14:46 +0300

    eslint and lint command setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
+const App = () => (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
@@ -10,9 +8,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
-  }
-}
+      </div>);
 
 export default App;
