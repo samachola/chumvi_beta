817a58e6f0496df4f56b76b2ed0b8f25f8315776
Sam Achola <sam.achola@live.com>
2018-04-04 11:14:29 +0300

    fix url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:5000/api-v0';
+const API_URL = 'https://chumviapi.herokuapp.com/api-v0';
 export default {
     user: {
       login: data => axios.post(`${API_URL}/auth/login`, data).then(response => response.data),
